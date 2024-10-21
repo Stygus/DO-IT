2344b68b9789cb390ea39eea011b8120765a77b3
Stygus <kuba.tokarczyk@onet.eu>
2024-10-21 22:17:56 +0200

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DOIT
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DOIT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
