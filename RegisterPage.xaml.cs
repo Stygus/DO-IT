4e14d0a7ead977543ee6935818df3865b7ab5089
kluciooo <dominikzucho@gmail.com>
2024-11-04 15:48:49 +0100

    Rejestracja - finałowy wygląd

--- RegisterPage.xaml.cs
+++ RegisterPage.xaml.cs
@@ -1,12 +1,38 @@
 using DOIT.ViewModels;
+using Xceed.Maui.Toolkit;
+using Xceed.Maui.Toolkit.Themes.Brushes;
 
 namespace DOIT;
 
 public partial class RegisterPage : ContentPage
 {
-    public RegisterPage()
+    private INavigation _navigation;
+    public RegisterPage(INavigation navigation)
     {
         InitializeComponent();
+        this._navigation = navigation;
         BindingContext = new RegisterViewModel(Navigation);
+    }
+
+
+
+    private void Button_PointerDown(object sender, EventArgs e)
+    {
+        if (sender is Xceed.Maui.Toolkit.Button button)
+        {
+            button.Background = Color.FromHex("#FFBF59");
+        }
+
+    }
+
+    private void Button_PointerUp(object sender, EventArgs e)
+    {
+        Xceed.Maui.Toolkit.Button button = sender as Xceed.Maui.Toolkit.Button;
+        button.Background = Color.FromArgb("1a1e23");
+    }
+
+    public async void OnLabelTapped(object sender, EventArgs e)
+    {
+        await this._navigation.PopAsync();
     }
 }
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -5,18 +5,22 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xceed;
+using Xceed.Maui.Toolkit;
 
 namespace DOIT.ViewModels
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     internal class RegisterViewModel : INotifyPropertyChanged
     {
+
         public string webApiKey = "AIzaSyA_I74E8Vc9HXg6P5b2Qe4bNzRYCll8fuk";
 
         private INavigation _navigation;
-        private string email;
-        private string password;
-        private string repassword;
-        private string nick;
+        private string email = "";
+        private string password = "";
+        private string repassword = "";
+        private string nick = "";
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -29,6 +33,7 @@ namespace DOIT.ViewModels
                 RaisePropertyChanged("Email");
             }
         }
+       
 
         public string Password
         {
@@ -55,6 +60,8 @@ namespace DOIT.ViewModels
             }
         }
 
+
+
         public Command RegisterUser { get; }
 
         private void RaisePropertyChanged(string v)
@@ -71,15 +78,16 @@ namespace DOIT.ViewModels
 
         private async void RegisterUserTappedAsync(object obj)
         {
-            if (password.Length <= 6)
+            if (password.Length <= 6 || password == null)
             {
                 App.Current.MainPage.DisplayAlert("", "Za krótkie hasło", "OK");
+                return;
             }
-            else if (password != repassword)
+            if (password != repassword)
             {
                 App.Current.MainPage.DisplayAlert("", "Podane hasła nie są identyczne", "OK");
+                return;
             }
-            else
                 try
                 {
                     var authProvider = new FirebaseAuthProvider(new FirebaseConfig(webApiKey));
@@ -112,8 +120,9 @@ namespace DOIT.ViewModels
                         return;
                     }
 
-                    await App.Current.MainPage.DisplayAlert("Alert", "Inny błąd", "OK");
+                    await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
                 }
         }
+
     }
 }
